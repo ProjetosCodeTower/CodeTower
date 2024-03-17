ca83ec6d53fdcc7c587141e7cb1f2c4ecdb2a857
carloscurcino <carlosdanielf541@gmail.com>
2024-03-17 18:35:46 -0300

    Feat: doing responsivity on navbar, adding hamburguer button to open a sheet

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { Menu } from 'lucide-react'
+
 
 export const Navbar = () => {
     return (
-        <div className='container fixed flex items-center justify-around bg-white/80 shadow backdrop-blur-sm backdrop-filter'>
+        <div className='w-full fixed flex items-center justify-around bg-white/80 shadow backdrop-blur-sm backdrop-filter'>
             <nav className='w-screen flex justify-between z-20 items-center py-4 px-10 text-dark'>
                 <div className='flex text-center items-center justify-center'>
                     <Image
@@ -16,13 +22,58 @@ export const Navbar = () => {
                     />
                     <p className='inline align-middle text-2xl font-light uppercase'>CODE <span className='font-bold'>TOWER</span></p>
                 </div>
-                <ul className='flex items-center justify-between gap-4'>
-                    <li>Home</li>
-                    <li>Serviços</li>
-                    <li>Portfólio</li>
-                    <li>Sobre</li>
-                    <li><a href="#" className='font-poppins px-3 py-2 shadow hover:opacity-75 bg-[#292D34] text-white hover:bg-primary rounded-lg'>Contato</a></li>
+                <ul className='hidden lg:flex items-center justify-between gap-4'>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#servicos">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfólio</a>
+                    </li>
+                    <li>
+                        <a href="#sobre">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#contato" className='font-poppins px-3.5 py-2.5 shadow hover:opacity-75 bg-[#292D34] text-white hover:bg-primary rounded-lg'>Contato</a>
+                    </li>
                 </ul>
+
+                <Sheet>
+                    <SheetTrigger asChild className='flex lg:hidden'>
+                        <Button variant="outline" className='border-none'><Menu /></Button>
+                    </SheetTrigger>
+                    <SheetContent className='flex lg:hidden justify-center'>
+                        <ul className='flex flex-col items-center gap-4 text-xl mt-6'>
+                            <li>
+                                <SheetClose asChild>
+                                    <a href="#home">Home</a>
+                                </SheetClose>
+                            </li>
+                            <li>
+                                <SheetClose asChild>
+                                    <a href="#servicos">Serviços</a>
+                                </SheetClose>
+                            </li>
+                            <li>
+                                <SheetClose asChild>
+                                    <a href="#portfolio">Portfólio</a>
+                                </SheetClose>
+                            </li>
+                            <li>
+                                <SheetClose asChild>
+                                    <a href="#sobre">Sobre</a>
+                                </SheetClose>
+                            </li>
+                            <li>
+                                <SheetClose asChild>
+                                    <a href="#contato" className='font-poppins px-3.5 py-2.5 shadow hover:opacity-75 bg-[#292D34] text-white hover:bg-primary rounded-lg'>Contato</a>
+                                </SheetClose>
+                            </li>
+                        </ul>
+                    </SheetContent>
+                </Sheet>
             </nav>
         </div>
     )
