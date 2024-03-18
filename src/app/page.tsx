399436a33394e4473ff23319ef9f34abcade02cb
carloscurcino <carlosdanielf541@gmail.com>
2024-03-17 21:49:02 -0300

    Feat: doing the home section and adding Poppins as fontFamily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      HELLO WWWORLD
-    </div>
+    <main className="container font-poppins">
+      <section id="home" className="h-screen flex flex-col items-center justify-center">
+        <p className="text-center font-semibold uppercase text-primary">Bem vindos à Code Tower</p>
+        <h1 className="my-4 text-center text-4xl font-semibold text-dark lg:text-6xl">Inovação em Tecnologia</h1>
+        <p className="text-center uppercase">Somos um grupo de estudantes focado em solucionar problemas da sociedade.</p>
+      </section>
+      <section id="servicos">
+        Serviços
+      </section>
+      <section id="portfolio">
+        Portfólio
+      </section>
+      <section id="sobre">
+        Sobre
+      </section>
+      <section id="contato">
+        Contato
+      </section>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+  ],
   prefix: "",
   theme: {
     container: {
@@ -16,62 +16,28 @@ const config = {
       screens: {
         "2xl": "1400px",
       },
+      // container: {
+      //   center: true,
+      //   padding: "1rem",
+      // },
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+        black: '#000',
+        white: '#fff',
+        dark: '#292d34',
+        primary: '#407bff',
+        secondary: '#ff6b6b',
+        tertiary: '#7bc678',
+        quaternary: '#f2994a',
+        gray: {
+          default: '#828282',
+          soft: '#f2f2f2'
+        }
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
