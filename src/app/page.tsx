df95a144c93b49be21774783c643790d80fc92e9
carloscurcino <carlosdanielf541@gmail.com>
2024-03-25 23:16:24 -0300

    Fix: using next fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="container font-poppins">
+    <main className="container">
       <section id="home" className="h-screen flex flex-col items-center justify-center">
         <p className="text-center font-semibold uppercase text-primary">Bem vindos à Code Tower</p>
         <h1 className="my-4 text-center text-4xl font-semibold text-dark lg:text-6xl">Inovação em Tecnologia</h1>
         <p className="text-center uppercase">Somos um grupo de estudantes focado em solucionar problemas da sociedade.</p>
       </section>
-      <section id="servicos">
-        Serviços
+      <section id="servicos" className="h-screen items-center justify-center">
+        <div className="grid grid-cols-4 items-center justify-center text-center">
+          <p>teste</p>
+          <p>teste</p>
+          <p>teste</p>
+          <p>teste</p>
+        </div>
       </section>
       <section id="portfolio">
         Portfólio
---
+++ src/components/service-icon.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ServiceIcon = () => {
+    return (
+        <div>service-icon</div>
+    )
+}
