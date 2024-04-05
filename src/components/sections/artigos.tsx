4f60d59858e38f21d11b4d5119def53db9303a0f
carloscurcino <carlosdanielf541@gmail.com>
2024-04-05 03:51:52 -0300

    Fix: card size pattern

--- src/components/sections/artigos.tsx
+++ src/components/sections/artigos.tsx
@@ -51,11 +51,11 @@ export const Artigos = () => {
             <p className="mb-2 text-center text-sm font-semibold text-primary">Atualizações e Novidades</p>
             <h3 className="mb-8 text-center text-2xl">Artigos recentes</h3>
 
-            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center px-8">
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-4 items-stretch justify-center px-8">
                 {blogs.length > 0 ? (
                     blogs.map((blog) => (
                         <a key={blog.id} href={blog.url} target="_blank">
-                            <Card className="border-none flex-1">
+                            <Card className="border-none flex-1 h-full flex flex-col justify-between">
                                 <CardHeader>
                                     <Image width={0} height={0} sizes='100vw' className="mb-4 h-[290px] rounded-md w-full object-cover lg:h-48" src={blog.cover_image ?? "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} title="Membro 1 - Presidente" alt="Membro 1 - Presidente" />
                                     <CardTitle className="text-sm font-bold uppercase text-primary">{blog.title}</CardTitle>
