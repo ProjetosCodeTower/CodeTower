d42eca2827451d6fbba22c87266be7b34faaebbb
carloscurcino <carlosdanielf541@gmail.com>
2024-04-07 02:44:12 -0300

    Fix: adding dynamic animation for card projects

--- src/components/sections/portfolio.tsx
+++ src/components/sections/portfolio.tsx
@@ -43,8 +43,8 @@ export const Portfolio = () => {
             <h3 className="mb-4 text-left text-2xl">Conheça nosso trabalho</h3>
             <div className="projects-grid grid grid-cols-1 gap-4 rounded-lg py-4 md:grid-cols-12">
                 {projects.length > 0 && (
-                    projects?.map((project) => (
-                        <div key={project.id} className="group relative flex h-full flex-1 flex-col overflow-hidden rounded-lg aos-init aos-animate" data-aos="fade-left">
+                    projects?.map((project, index) => (
+                        <div key={project.id} className="group relative flex h-full flex-1 flex-col overflow-hidden rounded-lg aos-init aos-animate" data-aos={`${index % 4 < 2 ? 'fade-left' : 'fade-right'}`}>
                             <Image className="h-72 w-full object-cover transition-all duration-300 ease-in-out group-hover:scale-105" src={project.imagem.url} alt={project.nomeDoProjeto} title={project.nomeDoProjeto} width="640" height="427" loading="lazy" />
                             <div className="absolute -bottom-full left-0 w-full bg-white/60 px-8 py-4 transition-all duration-300 ease-in-out group-hover:bottom-0">
                                 <span className="text-xs font-semibold uppercase text-primary">{project.tipoDeProjeto}</span>
