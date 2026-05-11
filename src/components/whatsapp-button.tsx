6647b5af1f0f083219dbf009d1bd4650b983cfb4
Guilherme do Vale Flávio <guilhermevale109@gmail.com>
2026-05-10 22:24:49 -0300

    colocando o numero certo no botão d whatsapp

--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -4,7 +4,7 @@ import { WhatsappLogo } from '@phosphor-icons/react'
 import React from 'react'
 
 export const WhatsappButton = () => {
-    const phone = "5562999535210"
+    const phone = "5519997303641"
     const text = "Olá, vi o site da Code Tower e gostaria de conversar sobre a possibilidade de um serviço de:______"
     return (
         <a target='_blank' href={`https://api.whatsapp.com/send/?phone=${phone}&text=${text}&type=phone_number&app_absent=0`} className='fixed bottom-5 right-5 bg-[#25d366] hover:bg-[#219c4e] cursor-pointer rounded-full'>
